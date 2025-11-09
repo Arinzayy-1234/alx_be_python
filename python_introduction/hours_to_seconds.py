f7d6813777c10a86d448ef7668d8fadeaa8bb9ed
Arinzayyy - Kris <103210483+Arinzayy-1234@users.noreply.github.com>
2025-11-09 23:02:08 +0100

    ALX checker ->  - hours_to_seconds.py doesn't contain: ["seconds = hours * 3600"] - hours_to_seconds.py doesn't contain: ["seconds=hours*3600"] - hours_to_seconds.py doesn't contain: ["seconds = 3600 * hours"] - hours_to_seconds.py doesn't contain: ["seconds=3600*hours"]

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -4,7 +4,7 @@ hours = 2
 # Calculate the number of seconds.
 # There are 60 minutes in an hour and 60 seconds in a minute (60 * 60 = 3600 seconds/hour)
 SECONDS_PER_HOUR = 3600
-seconds = hours * SECONDS_PER_HOUR
+seconds = hours * 3600  
 
 # Print the result in the specified format
 print(f"{hours} hour(s) is {seconds} seconds")
